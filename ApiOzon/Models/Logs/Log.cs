ab89c85ea37d89c4d0595659aa1fb4b308e815d3
krasvl <79515990+krasvl@users.noreply.github.com>
2021-07-20 16:06:33 +0300

    Добавил классы лога и уровня логирования

---
+++ ApiOzon/Models/Logs/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiOzon.Models.Logs
+{
+    public class Log
+    {
+        public Guid Id { get; set; }
+        public LogLevel LogLevel { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Text { get; set; }
+        public string Source { get; set; }
+    }
+}
