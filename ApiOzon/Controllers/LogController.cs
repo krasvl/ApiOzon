42a106b36dfb3890f6cee8de237d42dd410b22a5
krasvl <79515990+krasvl@users.noreply.github.com>
2021-07-20 15:49:36 +0300

    Добавьте файлы проекта.

---
+++ ApiOzon/Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiOzon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+
+        public LogController()
+        {
+
+        }
+
+        [HttpGet]
+        public void Get()
+        {   
+
+        }
+    }
+}
